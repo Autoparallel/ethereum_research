5fae7a2ab3bc273d063c8c9363eb2c9f600abdaf
Waylon Jepsen <waylonjepsen1@gmail.com>
2023-11-10 14:08:28 +0300

    neptune sponge construction

---
+++ src/neptune.rs
@@ -0,0 +1,17 @@
+use neptune::sponge::vanilla::{Mode, Sponge, SpongeTrait};
+use neptune::poseidon::PoseidonConstants;
+use blstrs::Scalar as Fr;
+use typenum::U2;
+
+fn main() {
+    // Define the constants for the Poseidon hash function
+    let constants: PoseidonConstants<Fr, U2> = PoseidonConstants::new_constant_length(0);
+
+    // Create a new Sponge object
+    let sponge = Sponge::new_with_constants(&constants, Mode::Duplex);
+
+    println!("Sponge object created: {:?}", sponge.state);
+
+    // Now you can use the sponge object
+    // ...
+}
